5a3d387c2261bf76d7c800b92ecd4c7673d94655
fazer1929 <abhishekagrawal8888@gmail.com>
2021-02-07 00:27:21 +0530

    Hand Detection Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const camRef = useRef(null);
+	const canvasRef = useRef(null);
+	const runHandpose = async () => {
+		const net = await handpose.load();
+		console.log("hanpose Loaded");
+		//Detect Hands
+		setInterval(() => {
+			detect(net);
+		}, 100);
+	};
+	const detect = async (net) => {
+		if (
+			typeof camRef.current !== "undefined" &&
+			camRef.current != null &&
+			camRef.current.video.readyState === 4
+		) {
+			// get video properties
+			const video = camRef.current.video;
+			const videoWidth = camRef.current.video.videoWidth;
+			const videoHeight = camRef.current.video.videoHeight;
+			//Set Video properties
+			camRef.current.video.width = videoWidth;
+			camRef.current.video.height = videoHeight;
+			// set canvas properties
+			canvasRef.current.width = videoWidth;
+			canvasRef.current.height = videoHeight;
+
+			//Make Detections
+			const hand = await net.estimateHands(video);
+			console.log(hand);
+		}
+	};
+	runHandpose();
+	return (
+		<div>
+			<Webcam
+				ref={camRef}
+				mirrored
+				style={{
+					position: "absolute",
+					marginLeft: "auto",
+					marginRight: "auto",
+					right: 0,
+					zIndex: 9,
+					width: 320,
+					height: 240,
+					textAlign: "center",
+				}}
+			></Webcam>
+			<canvas
+				ref={canvasRef}
+				style={{
+					position: "absolute",
+					left: 0,
+					right: 0,
+					marginLeft: "auto",
+					marginRight: "auto",
+					zIndex: 9,
+					width: 640,
+					height: 480,
+					textAlign: "center",
+				}}
+			/>
+		</div>
+	);
 }
 
 export default App;
